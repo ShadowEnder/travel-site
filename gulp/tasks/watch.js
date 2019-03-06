c8947c1163887ac9ff7a6f6b3734a23c335b1ea3
ShadowEnder <groundon777@msn.com>
2019-03-06 16:58:14 -0500

    Started styling header.

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -8,7 +8,8 @@ var gulp = require('gulp'), // needed by both
     cssvars = require('postcss-simple-vars'),
     nested = require('postcss-nested'),
     cssImport = require('postcss-import'),
-    mixins = require('postcss-mixins');
+    mixins = require('postcss-mixins'),
+    hexrgba = require('postcss-hexrgba');
 
 // watch
 gulp.task('watch', function() {
@@ -39,7 +40,7 @@ gulp.task('styles', styles);
 function styles() {
   console.log("Styles task ran.");
   return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+    .pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba, autoprefixer]))
     .on('error', function (errorInfo) {
       console.log(errorInfo.toString());
       this.emit('end');
