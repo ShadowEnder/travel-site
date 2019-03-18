d052628c082e51bceac460030ef067562bcc9da5
ShadowEnder <groundon777@msn.com>
2019-03-18 04:14:10 -0400

    Disable gh pages.

--- gulp/tasks/gh-pages.js
+++ gulp/tasks/gh-pages.js
@@ -1,4 +1,4 @@
-const {src, task}= require('gulp');
-const ghPages = require('gulp-gh-pages');
+//const {src, task}= require('gulp');
+//const ghPages = require('gulp-gh-pages');
 
-task('deploy', () => src('./dist/**/*').pipe(ghPages()));
+//task('deploy', () => src('./dist/**/*').pipe(ghPages()));
